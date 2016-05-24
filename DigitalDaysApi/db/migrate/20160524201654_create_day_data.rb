c08ef3dea11a14875d391d5368738b1762c103ef
Frankie <email": null>
2016-05-24 13:28:41 -0700

    Create migrations

--- DigitalDaysApi/db/migrate/20160524201654_create_day_data.rb
+++ DigitalDaysApi/db/migrate/20160524201654_create_day_data.rb
@@ -1,6 +1,12 @@
 class CreateDayData < ActiveRecord::Migration
   def change
     create_table :day_data do |t|
+      t.integer :user_id, null: false
+      t.string :weather
+      t.integer :feeling
+      t.string :food_eaten
+      t.integer :productivity
+      t.string :note
 
       t.timestamps null: false
     end
--- DigitalDaysApi/db/migrate/20160524201741_create_day_notes.rb
+++ DigitalDaysApi/db/migrate/20160524201741_create_day_notes.rb
@@ -1,6 +1,8 @@
 class CreateDayNotes < ActiveRecord::Migration
   def change
     create_table :day_notes do |t|
+      t.integer :user_id
+      t.string :entry
 
       t.timestamps null: false
     end
